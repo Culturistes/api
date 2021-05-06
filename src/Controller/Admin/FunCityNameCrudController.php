c508d971680cb8c4055f320d22d0da0ea492e409
remi <remi.rucojevic@gmail.com>
2021-05-06 16:38:02 +0200

    add description to Question and FunCityName

--- src/Controller/Admin/FunCityNameCrudController.php
+++ src/Controller/Admin/FunCityNameCrudController.php
@@ -36,6 +36,7 @@ class FunCityNameCrudController extends AbstractCrudController
             Field::new('longitude')->setLabel('Longitude')->setFormTypeOptions([
                 'block_name' => 'custom',
             ]),
+            Field::new('description')->setLabel('Petite explication ?')->hideOnIndex(),
             AssociationField::new('regions')->setLabel('Region associée')->onlyOnForms(),
             Field::new('RegionName')->setLabel('Region associée')->hideOnForm(),
             Field::new('active')->setHelp('Permet de désactiver une question, décocher pour l\'empêcher d\'apparaitre dans le jeu sans la supprimer'),
